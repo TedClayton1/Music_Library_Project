e3fb14d9870b36672b22deefccd1f188b0328bcc
Ted Clayton <ted_clayton@hotmail.com>
2021-11-07 11:38:04 -0500

    I added 5 rock songs to my database.

---
+++ musiclibrary/musiclibrary/local_settings.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+SECRET_KEY = 'django-insecure-5=_@*wnfydo27$+w5@ar^mhnt$^g)p#kt5)&8ub23$f_m$l&*z'
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'music_library_database',
+        'USER': 'root',
+        'PASSWORD': 'BoxofScones456#',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
+
+
